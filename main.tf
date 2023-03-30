60a07db068a2e838f1315c13560ccf37b1b5f39c
Luisj5231 <luis.gciagtz@outlook.com>
2023-03-29 18:21:19 -0600

    resources added to main.tf

--- main.tf
+++ main.tf
@@ -10,19 +10,38 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  
 }
 
+#VPC
+resource "aws_vpc" "principal" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.principal.id
+  cidr_block = "10.9.0.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+# VM
 resource "aws_instance" "self" {
-  ami                         = "ami-0b0af3577fe5e3532"
-  instance_type               = "t2.micro"
-  key_name                    = "dummyWSK"
-  subnet_id                   = "subnet-0d0b48a8f513bbb51"
+  ami                         = var.ami_ID
+  instance_type               = var.ec2_instance_type
+  key_name                    = var.key_name_ec2
+  subnet_id                   = aws_subnet.main.id
   root_block_device {
     volume_type = "gp2"
     volume_size = 30
     encrypted   = false
   }
   tags = {
-    Name = "my-instance"
+    Name = var.instance_name
   }
 }
+
+# boot, bootefi, lvm2
